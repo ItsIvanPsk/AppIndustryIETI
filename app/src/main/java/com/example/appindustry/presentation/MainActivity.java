dac19387c74175acb7f5de8b5ba35fa27bdc65be
Ivan Figueredo <73289877+ItsIvanPsk@users.noreply.github.com>
2022-11-10 16:35:15 +0100

    [SP0108] - Main Menu UI (#1)
    [SP0108] - Main Menu UI

---
+++ app/src/main/java/com/example/appindustry/presentation/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.appindustry.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.appindustry.R;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton logIn_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logIn_btn.findViewById(R.id.main_button_login);
+
+        settupButtonsListeners();
+    }
+
+
+    private void settupButtonsListeners(){
+
+        logIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogIn();
+            }
+        });
+
+    }
+
+    private void userLogIn() {
+
+    }
+
+}
