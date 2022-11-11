4aa0b4daff6431bad2641e99d49147121f12ddfd
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2022-11-11 14:52:29 +0100

    WebSockets

---
+++ app/src/main/java/com/example/AppIndustry/presentation/MainDashboard.java
@@ -0,0 +1,16 @@
+package com.example.AppIndustry.presentation;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.appindustry.R;
+
+public class MainDashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_dashboard);
+
+    }
+}
--- app/src/main/java/com/example/appindustry/presentation/MainActivity.java
+++ app/src/main/java/com/example/appindustry/presentation/MainActivity.java
@@ -1,41 +1,27 @@
-package com.example.appindustry.presentation;
+package com.example.AppIndustry.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import com.example.AppIndustry.data.*;
 import com.example.appindustry.R;
 
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.exceptions.WebsocketNotConnectedException;
-import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    int port = 8888;
-    String location = "10.0.2.2";
-    String uri = "ws://" + location + ":" + port;
-    WebSocketClient client;
-    TextView textView;
-    EditText inputText;
+    WebSockets client;
+    EditText usernameInput, passwordInput;
+    Button button;
+    static boolean validated = false;
 
     @Override
     protected void onStart() {
         super.onStart();
-        connecta();
     }
 
     @Override
@@ -44,54 +30,37 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.main_btn_login);
+        usernameInput = findViewById(R.id.main_input_username);
+        passwordInput = findViewById(R.id.main_input_password);
+
+        client = new WebSockets();
+        client.connecta();
+
+        setButtonListeners();
     }
 
-    protected void envia (boolean salt) {
-        String missatge = String.valueOf(inputText.getText()).replaceAll("\n", "");
-        if (missatge.compareTo("") != 0) {
-            if (salt) {
-                textView.append(missatge + "\n");
-            } else {
-                textView.append(missatge);
-            }
-            inputText.setText("");
-            try {
-                client.send(missatge);
-            } catch (WebsocketNotConnectedException e) {
-                System.out.println("Connexió perduda ...");
-                connecta();
-            }
-        }
-    }
-
-    public void connecta () {
-        try {
-            client = new WebSocketClient(new URI(uri), (Draft) new Draft_6455()) {
-                @Override
-                public void onMessage(String message) { onMessageListener(message); }
-
-                @Override
-                public void onOpen(ServerHandshake handshake) {
-                    System.out.println("Connected to: " + getURI());
+    public void setButtonListeners(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client.envia(
+                        usernameInput.getText().toString() + "#" + passwordInput.getText().toString()
+                );
+                if (validated){
+                    String username = usernameInput.getText().toString();
+                    Intent intent = new Intent(MainActivity.this,MainDashboard.class);
+                    intent.putExtra(username, username);
+                    startActivity(intent);
                 }
-
-                @Override
-                public void onClose(int code, String reason, boolean remote) {
-                    System.out.println("Disconnected from: " + getURI());
-                }
-
-                @Override
-                public void onError(Exception ex) { ex.printStackTrace(); }
-            };
-            client.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            System.out.println("Error: " + uri + " no és una direcció URI de WebSocket vàlida");
-        }
+            }
+        });
     }
 
-    protected void onMessageListener (String message) {
-        textView.append(message + "\n");
+    public static void setValidated(boolean val){
+        validated = val;
     }
+
+
 
 }
